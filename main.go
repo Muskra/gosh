78b63c6c45d4326f9eedb2c8188fd2de857de207
Muskra <76000573+Muskra@users.noreply.github.com>
2023-03-19 15:09:41 +0100

    main.go update
    What i have added today:
    	- Test switch that bruteforce the program with lots of inputs 
    		- actually it generates all chains of chars between a specified range with the characters you specified
    		- it also skip the normal loop, if i want to release the program i want to remove completely any test code, so it have it's own function
    	- string and special character lexing, we firstly separate each strings from other words, then separate all punctuation from the rest
    		- note that the program just printout the result at the moment, it's wip actually so there is no executor to launch commands, just parsing for now

--- main.go
+++ main.go
@@ -10,27 +10,45 @@ import (
   // customs
   "gsh/std"
 )
-
-// main script, it loops into an infinite command prompt.
+// main function
+// here you can define a Characterset variable and a boolean variable Test to verify if the syntax you have created is handling normally or not by the program. You can also set the lenght of the words you want to test but be carefull since it can generate performance issues i'm not responsible for any damage done with this
+// if the Test variable is false (as by default) program will launch normally
 func main() {
-	// program loop
-	for {
-    defer fmt.Println("syntax error!")
+  // variables
+  var Characterset string = " |&;\""; // doesn't work with "quotes" for some reason
+  // value is 6 by default
+  var SetLenght int = 6;
+  // value is false by default
+  var Test bool = false;
+  // if Test variable is false we launch program normally
+  if Test == false {
+    NormalLoop()
+  }
+  // if Test variable is true we launch the test loop
+  if Test == true {
+    TestLoop(Characterset, SetLenght)
+  }
+}
+// test loop, can be launched if Test variable is set to true
+func TestLoop(characterset string, lenght int) {
+  // declaration
+  var pmtList []string;
+  // generating each sentences from the GenerateCombinations() function with the given Characterset variable
+	for combination := range std.GenerateCombinations(characterset, lenght) {
+	  pmtList = append(pmtList, combination)
+	}
+  // program loop
+  for _, prompt := range pmtList {
+    //defer fmt.Println("syntax error!")
     // variables
-    var prompt string = "";
     var commandList []string;
-
-  	fmt.Printf("$_ ")
-		// read the user's input
-    // we use pointers in ReadingInput() to improve performances
-    ReadingInput(&prompt)
     // loops if the user typed enter
     if prompt == "\n" {
       continue;
     }
     // handling the "\" character which means the command is multiline
     for bl := strings.HasSuffix(strings.TrimSuffix(prompt, "\n"), "\\"); bl == true; {
-      defer fmt.Println("syntax error!")
+      //defer fmt.Println("syntax error!")
       fmt.Print("> ")
       ReadingInput(&prompt)
       // a check is required to break the loop
@@ -38,9 +56,11 @@ func main() {
         break;
       }
     }
+    fmt.Printf("sentence: \n{%s}", prompt)
     // this is where we call our next function
     commandList = SentenceSplitter(prompt)
-    //fmt.Println("")
+    // printout result
+    fmt.Println("\nfinal_command: ")
     if len(commandList) > 0 {
       for _, cmd := range commandList {
         fmt.Printf("{%s}", cmd)
@@ -49,6 +69,44 @@ func main() {
     fmt.Println("")
   }
 }
+// Entrypoint of the program, will be launch as normal behavior
+func NormalLoop() {
+  	// program loop
+  	for {
+      defer fmt.Println("syntax error!")
+      // variables
+      var prompt string = "";
+      var commandList []string;
+
+    	fmt.Printf("$_ ")
+  		// read the user's input
+      // we use pointers in ReadingInput() to improve performances
+      ReadingInput(&prompt)
+      // loops if the user typed enter
+      if prompt == "\n" {
+        continue;
+      }
+      // handling the "\" character which means the command is multiline
+      for bl := strings.HasSuffix(strings.TrimSuffix(prompt, "\n"), "\\"); bl == true; {
+        defer fmt.Println("syntax error!")
+        fmt.Print("> ")
+        ReadingInput(&prompt)
+        // a check is required to break the loop
+        if bl := strings.HasSuffix(strings.TrimSuffix(prompt, "\n"), "\\"); bl == false {
+          break;
+        }
+      }
+      // this is where we call our next function
+      commandList = SentenceSplitter(prompt)
+      if len(commandList) > 0 {
+        for _, cmd := range commandList {
+          fmt.Printf("{%s}", cmd)
+      }
+    }
+    fmt.Println("")
+    }
+}
+
 // ReadingInput
 // input reader
 func ReadingInput(xPrompt *string) {
@@ -64,22 +122,64 @@ func ReadingInput(xPrompt *string) {
 func SentenceSplitter(sentence string) []string {
   // defines the ret variable
   var ret []string;
-  // split our sentence by " " (spaces)
-  xSentence := strings.Fields(sentence)
-  // here we lookup throught all words from the sentence and return a list of each words separated from their special characters
-  for _, wrd := range xSentence {
-    wrd = strings.TrimSpace(wrd)
-    //wrd = strings.TrimPrefix(strings.TrimSuffix(wrd, " "), " ")
-    spltWrd := SpecialCharLookup(wrd)
-    if len(spltWrd) >= 1 {
-      for _, xWrd := range spltWrd {
-        ret = append(ret, xWrd)
-      }
+  var xRet []string;
+  // used for temporary or buffering
+  var temp string;
+  // bool variable, used as a check for the presence of "quotes"
+  var bl bool;
+  // iterate throught the whole sentence and isolate each strings from other words within a slice
+  for index, ch := range sentence {
+    if string(ch) == "\"" && bl == false {
+      ret = append(ret, temp)
+      bl = true
+      temp = string(ch)
+      continue;
+    }
+    if string(ch) == "\"" && bl == true {
+      temp = temp+string(ch)
+      ret = append(ret, temp)
+      bl = false
+      temp = ""
+      continue;
+    }
+    if string(ch) != "\"" && bl == false {
+      temp = temp+string(ch)
+    }
+    if string(ch) != "\"" && bl == true {
+      temp = temp+string(ch)
+    }
+    if index == len(sentence)-1 {
+      ret = append(ret, temp)
     }
   }
-  return ret;
+  // here we are iterating again, but this time throught the slice, and we are isolating special characters, say punctuation from the other words
+  // we also won't check strings here since they already are checked beforehand
+  for index, word := range ret {
+    if len(word) > 1 {
+      if strings.Contains(string(word), "\"") == false {
+        for _, wrd := range strings.Fields(word) {
+          wrd = strings.TrimSpace(wrd)
+          spltWrd := SpecialCharLookup(wrd)
+          for _, xWrd := range spltWrd {
+            xRet = append(xRet, xWrd)
+          }
+        }
+      } else if strings.Contains(string(word), "\"") == true {
+        xRet = append(xRet, string(word))
+      } else {
+        fmt.Println("syntax error!")
+      }
+    } else if len(word) == 1 || index == len(ret)-1 {
+      xRet = append(xRet, word)
+    } else {
+      fmt.Print("\nparse error!\n")
+      break;
+    }
+  }
+  return xRet
 }
 
+// checks wether we have special characters or not
 func SpecialCharLookup(word string) []string {
   // declarations
   // buffer
@@ -115,11 +215,12 @@ func SpecialCharLookup(word string) []string {
       }
     }
     // we have only one char in the string
-  } else if len(word) == 1 {
+  } 
+  if len(word) == 1 {
     ret = append(ret, word)
     // error trigger
-  } else {
-    fmt.Println("parse error!")
-  }
+  } /*else {
+    fmt.Print("\nparse error!\n")
+  }*/
   return ret
 }
